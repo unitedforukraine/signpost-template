c683621bb58950a431d600ee89d58c81dd678b86
knightlore <gnightshade@gmail.com>
2024-04-09 17:40:09 -0300

    Load Zendesk Content

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,15 +4,28 @@ import { api } from "./api"
 import { app } from "./app"
 
 export class DB extends Dexie {
+
+  zendeskSections!: Dexie.Table<ZendeskSection, number>
+  zendeskCategories!: Dexie.Table<ZendeskCategory, number>
+  articles!: Dexie.Table<ZendeskArticle, number>
   services!: Dexie.Table<Service, number>
   providers!: Dexie.Table<Provider, number>
 
   constructor() {
     super("db")
     this.version(1).stores({
+      zendeskCategories: 'id',
+      zendeskSections: 'id',
+      articles: 'id',
+
       services: 'id, date_created, date_updated',
       providers: 'id',
     })
+
+    this.articles = this.table("articles")
+    this.zendeskCategories = this.table("zendeskCategories")
+    this.zendeskSections = this.table("zendeskSections")
+
     this.services = this.table("services")
     this.providers = this.table("providers")
   }
@@ -35,6 +48,29 @@ export class DB extends Dexie {
     }
     console.log("Local Providers Found: ", dbs.length)
     return dbs.length
+  }
+
+  async loadZendeskContent(): Promise<number> {
+
+    const cats = (await this.zendeskCategories.toArray()) || []
+    if (cats.length > 0) {
+      const r = cats.reduce((a, b) => { a[b.id] = b; return a }, {})
+      app.data.zendesk.categories = r as any
+    }
+
+    const sects = (await this.zendeskSections.toArray()) || []
+    if (sects.length > 0) {
+      const r = sects.reduce((a, b) => { a[b.id] = b; return a }, {})
+      app.data.zendesk.sections = r as any
+    }
+
+    const arts = (await this.articles.toArray()) || []
+    if (arts.length > 0) {
+      const r = arts.reduce((a, b) => { a[b.id] = b; return a }, {})
+      app.data.zendesk.articles = r as any
+    }
+
+    return arts.length
   }
 
   async updateServices() {
@@ -69,6 +105,32 @@ export class DB extends Dexie {
       }
     }
     console.log("Providers Database Updated!")
+  }
+
+  async updateArticles() {
+    console.log("Updating Articles Database...")
+    const content = await api.getArticles(app.country)
+
+    if (content) {
+      console.log(`Saving Articles`)
+      for (const key in content) {
+        const cat = content[key]
+        await this.zendeskCategories.put(cat, cat.id)
+        for (const key in cat.sections) {
+          const sec = cat.sections[key]
+          sec.category = cat.id
+          await this.zendeskSections.put(sec, sec.id)
+          for (const key in sec.articles) {
+            const art = sec.articles[key]
+            art.section = sec.id
+            art.category = cat.id
+            await this.articles.put(art, art.id)
+          }
+        }
+      }
+    }
+
+    console.log("Articles Database Updated!")
   }
 
 }
